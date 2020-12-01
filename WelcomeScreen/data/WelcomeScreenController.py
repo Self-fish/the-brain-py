e5c67df6f6253e17bc4fdda5d462ac38c974ce33
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-01 12:08:36 +0100

    Centering the WelcomeMessage

--- WelcomeScreen/data/WelcomeScreenController.py
+++ WelcomeScreen/data/WelcomeScreenController.py
@@ -4,4 +4,4 @@ from WelcomeScreen.data import I2C_LCD_driver
 def show_welcome_message():
     mylcd = I2C_LCD_driver.lcd()
     mylcd.lcd_display_string("Hello Pablo!", 2, 4)
-    mylcd.lcd_display_string("Initialising...", 3, 4)
+    mylcd.lcd_display_string("Initialising...!", 3, 2)

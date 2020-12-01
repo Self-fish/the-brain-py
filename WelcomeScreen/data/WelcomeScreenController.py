e5d06e7f894f2c028f8dcac4d66810e1832cc4d6
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-01 12:06:48 +0100

    Creating Welcome Message

--- WelcomeScreen/data/WelcomeScreenController.py
+++ WelcomeScreen/data/WelcomeScreenController.py
@@ -3,6 +3,5 @@ from WelcomeScreen.data import I2C_LCD_driver
 
 def show_welcome_message():
     mylcd = I2C_LCD_driver.lcd()
-    mylcd.lcd_display_string("Hello World!", 3, 4)
-    print("Welcome Pablo!")
-    print("Please Wait!")
+    mylcd.lcd_display_string("Hello Pablo!", 2, 4)
+    mylcd.lcd_display_string("Initialising...", 3, 4)

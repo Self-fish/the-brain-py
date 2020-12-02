54dbe1d6db04957b89dde2aeed4a84307dac4a5c
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-02 12:36:54 +0100

    Wiring the container in the main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import sys
+
+from WelcomeScreen.Containers import Container
 from WelcomeScreen.domain import WelcomeScreenUseCase
 
-
 if __name__ == '__main__':
+    container = Container()
+    container.wire(modules=[sys.modules[__name__]])
     WelcomeScreenUseCase.show_screen()
 

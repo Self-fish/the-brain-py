3a528579a5dc14c9c3fdaff2948b61d5b96064b6
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-02 20:44:41 +0100

    Using wiringpi2 for handling the relay

--- HandleLights/data/repository/Controller.py
+++ HandleLights/data/repository/Controller.py
@@ -1,16 +1,17 @@
-import RPi.GPIO as GPIO
+import wiringpi2
+
 
 class LightController:
 
     def __init__(self):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(11, GPIO.OUT)
+        self.io = wiringpi2.GPIO(wiringpi2.GPIO.WPI_MODE_PINS)
+        self.io.pinMode(11, self.io.OUTPUT)
 
     def turn_on_lights(self):
-        GPIO.output(11, GPIO.LOW)
+        self.io.digitalWrite(11, self.io.HIGH)
         print("Encedemos las luces")
 
 
     def turn_off_lights(self):
-        GPIO.output(11, GPIO.HIGH)
+        self.io.digitalWrite(11, self.io.LOW)
         print("Apagamos las luces")

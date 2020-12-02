3076779cc4d63e139b6cd20e70ff179938755ca9
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-02 12:01:30 +0100

    Adding missing dependency of Containers

---
+++ WelcomeScreen/Containers.py
@@ -0,0 +1,9 @@
+from dependency_injector import providers, containers
+from WelcomeScreen.data import I2C_LCD_driver
+
+
+class Container(containers.DeclarativeContainer):
+
+    config = providers.Configuration()
+    lcd = providers.Singleton(I2C_LCD_driver.lcd)
+

c42d4affecee7986b087803a32971194e109e3e5
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-02 20:56:34 +0100

    Minor changes

--- HandleLights/domain/usecase/UseCase.py
+++ HandleLights/domain/usecase/UseCase.py
@@ -10,6 +10,8 @@ class HandleLightsUseCase:
     def handle_lights(self):
         io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_PINS)
         io.pinMode(0, io.OUTPUT)
+        print("Encedemos las luces")
         io.digitalWrite(0, io.HIGH)
         time.sleep(5)
+        print("Apagamos las luces")
         io.digitalWrite(0, io.LOW)

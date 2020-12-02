770022ecdd857d5468e7b598a84fed64427b7f17
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-02 20:46:13 +0100

    Minor change

--- HandleLights/data/repository/Controller.py
+++ HandleLights/data/repository/Controller.py
@@ -1,10 +1,10 @@
-import wiringpi2
+import wiringpi
 
 
 class LightController:
 
     def __init__(self):
-        self.io = wiringpi2.GPIO(wiringpi2.GPIO.WPI_MODE_PINS)
+        self.io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_PINS)
         self.io.pinMode(11, self.io.OUTPUT)
 
     def turn_on_lights(self):

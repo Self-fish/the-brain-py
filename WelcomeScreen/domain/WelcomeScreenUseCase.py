fb30c1303c2b78093340a5b04822072b1abbc2b9
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-02 13:41:52 +0100

    Minor changes

--- WelcomeScreen/domain/WelcomeScreenUseCase.py
+++ WelcomeScreen/domain/WelcomeScreenUseCase.py
@@ -5,8 +5,7 @@ from WelcomeScreen.Containers import Container
 from WelcomeScreen.data import I2C_LCD_driver
 from WelcomeScreen.data.WelcomeScreenController import WelcomeScreenController
 
-container = Container()
-container.wire(modules=[sys.modules[__name__]])
+
 
 @inject
 def show_screen(lcd: I2C_LCD_driver.lcd = Provide[Container.lcd]):
--- main.py
+++ main.py
@@ -9,5 +9,7 @@ from WelcomeScreen.domain import WelcomeScreenUseCase
 
 
 if __name__ == '__main__':
+    container = Container()
+    container.wire(modules=[sys.modules[__name__]])
     WelcomeScreenUseCase.show_screen()
 

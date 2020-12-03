e1191293910f81f250e1d332610ddcab59bc32c1
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-03 09:14:23 +0100

    Creating LocalDataSource and Preferences repository to know when we should turn on / off the lights

---
+++ HandleLights/data/datasource/LocalDataSource.py
@@ -0,0 +1,5 @@
+from HandleLights.domain.model.LightPreferences import LightPreferences
+
+
+def get_light_preferences():
+    return LightPreferences("15:00", "22:59")
--- HandleLights/domain/usecase/UseCase.py
+++ HandleLights/domain/usecase/UseCase.py
@@ -1,6 +1,11 @@
-import time, wiringpi
-
 from HandleLights.data.controller.Controller import LightsController
+from HandleLights.data.repository import Preferences
+from HandleLights.domain.model.LightPreferences import LightPreferences
+from datetime import datetime
+
+
+def should_turn_on_lights(current_time, light_preferences: LightPreferences):
+    return light_preferences.starting_hour <= current_time < light_preferences.finishing_hour
 
 
 class HandleLightsUseCase:
@@ -9,11 +14,14 @@ class HandleLightsUseCase:
         self.__controller = LightsController()
 
     def handle_lights(self):
-        #io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_PINS)
-        #io.pinMode(0, io.OUTPUT)
-        #print("Encedemos las luces")
-        #io.digitalWrite(0, io.LOW)
-        self.__controller.turn_on_lights()
-        time.sleep(5)
-        self.__controller.turn_off_lights()
-        #io.digitalWrite(0, io.HIGH)
+        current_time = datetime.now().strftime("%H:%M")
+        preferences = Preferences.get_light_preferences()
+        print(current_time)
+        print(preferences)
+
+        if should_turn_on_lights(current_time, Preferences.get_light_preferences()):
+            print("Encendemos la luces")
+        else:
+            print("Apagamos las luces")
+
+

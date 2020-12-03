48a994afaf5804823e60deb239bef56bf620b3ed
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-03 09:53:08 +0100

    Playing with TimeZone

--- HandleLights/domain/usecase/UseCase.py
+++ HandleLights/domain/usecase/UseCase.py
@@ -14,14 +14,18 @@ class HandleLightsUseCase:
         self.__controller = LightsController()
 
     def handle_lights(self):
+        now_utc = now_utc = datetime.now(timezone('UTC'))
+        print(now_utc)
+        now_berlin = now_utc.astimezone(timezone('Europe/Berlin'))
+        print(now_berlin)
         current_time = timezone('US/Pacific').localize(datetime.now())
         preferences = Preferences.get_light_preferences()
         print(current_time)
         print(preferences.starting_hour + ":" + preferences.finishing_hour)
 
-        if should_turn_on_lights(current_time, Preferences.get_light_preferences()):
-            print("Encendemos la luces")
-        else:
-            print("Apagamos las luces")
+        #if should_turn_on_lights(current_time, Preferences.get_light_preferences()):
+        #    print("Encendemos la luces")
+        #else:
+        #    print("Apagamos las luces")
 
 

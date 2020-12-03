cc3c445b0250a6c9320e387a484423d120abf3b9
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-03 09:35:34 +0100

    Adding timezone when getting current time

--- HandleLights/domain/usecase/UseCase.py
+++ HandleLights/domain/usecase/UseCase.py
@@ -2,7 +2,7 @@ from HandleLights.data.controller.Controller import LightsController
 from HandleLights.data.repository import Preferences
 from HandleLights.domain.model.LightPreferences import LightPreferences
 from datetime import datetime
-
+import pytz
 
 def should_turn_on_lights(current_time, light_preferences: LightPreferences):
     return light_preferences.starting_hour <= current_time < light_preferences.finishing_hour
@@ -14,10 +14,11 @@ class HandleLightsUseCase:
         self.__controller = LightsController()
 
     def handle_lights(self):
-        current_time = datetime.now().strftime("%H:%M")
+        timezone = pytz.timezone("Europe/Madrid")
+        current_time = timezone.localize(datetime.now()).strftime("%H:%M")
         preferences = Preferences.get_light_preferences()
         print(current_time)
-        print(preferences)
+        print(preferences.starting_hour + ":" + preferences.finishing_hour)
 
         if should_turn_on_lights(current_time, Preferences.get_light_preferences()):
             print("Encendemos la luces")

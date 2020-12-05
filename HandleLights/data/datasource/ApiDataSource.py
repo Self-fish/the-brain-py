273da6031091865f0c19bb4b6ef108918f71a7b8
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-05 18:08:25 +0100

    Using /proc/cpuinfo to read the serial number of the device

--- HandleLights/data/datasource/ApiDataSource.py
+++ HandleLights/data/datasource/ApiDataSource.py
@@ -1,17 +1,18 @@
 import requests
-from pyserial import serial
 
 
 def get_light_preferences():
 
-    a = serial.tools.list_ports.comports()
-    ports = []
-    for w in a:
-        ports.append((w.vid, w.device, w.serial_number))
-    i = 0
-    for w in ports:
-        print('%d)\t%s\t(USB VID=%04X)\t Serial#:=%s' % (i, w[1], w[0] if (type(w[0]) is int) else 0, w[2]))
-        i = i + 1
+    try:
+        f = open('/proc/cpuinfo', 'r')
+        for line in f:
+            if line[0:6] == 'Serial':
+                print(line[10:26])
+        f.close()
+
+    except:
+        print("Error!")
+
 
     preferences = requests.get("http://192.168.0.16:8080/preferences?deviceId=sf-000000009df9b726")
     if preferences.status_code !=200:

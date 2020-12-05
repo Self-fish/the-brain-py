eebe309ec7c1265689a4f35ed8bddf795b2777c6
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-05 18:42:51 +0100

    Playing with exceptions

--- HandleLights/data/repository/Preferences.py
+++ HandleLights/data/repository/Preferences.py
@@ -5,9 +5,6 @@ from HandleLights.data.datasource import LocalDataSource, ApiDataSource, NoApiPr
 def get_light_preferences():
     try:
         return ApiDataSource.get_light_preferences()
-    except NoApiPreferencesException:
+    except Exception:
         print("Oh shit! Problem with the API")
-        return LocalDataSource.get_light_preferences()
-    except NoSerialException:
-        print("Oh shit! Problem reading the Serial Number")
         return LocalDataSource.get_light_preferences()

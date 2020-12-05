01b5bf48a747be0f7b824a9cc3fa625715f499a7
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-05 11:39:01 +0100

    Minor change

--- HandleLights/data/datasource/ApiDataSource.py
+++ HandleLights/data/datasource/ApiDataSource.py
@@ -2,7 +2,7 @@ import requests
 
 
 def get_light_preferences():
-    preferences = requests.get("192.168.0.16:8080/preferences?deviceId=sf-000000009df9b726")
+    preferences = requests.get("http://192.168.0.16:8080/preferences?deviceId=sf-000000009df9b726")
     if preferences.status_code !=200:
         print("Fuck")
     else:

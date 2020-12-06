ce1844b72c7f111da6180b245ac0d05fc6c79272
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-06 12:07:47 +0100

    Minor change

--- MainScreen/data/controller/DHTController.py
+++ MainScreen/data/controller/DHTController.py
@@ -1,12 +1,13 @@
 import adafruit_dht
 import board
+import time
 
 DHT = 2
 
 
 def read_temperature():
-    dht_device = adafruit_dht.DHT22(board.D2)
+    dht_device = adafruit_dht.DHT22(board.D3)
     while True:
         temperature = dht_device.temperature
         humidity = dht_device.humidity
-        sleep(5) #Wait 5 seconds and read again
+        time.sleep(5) #Wait 5 seconds and read again

786af9c7d05bda482e5ae0318086a0909e3a1695
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-06 11:22:55 +0100

    Playing with DHT22 sensor library

--- MainScreen/data/controller/DHTController.py
+++ MainScreen/data/controller/DHTController.py
@@ -1,20 +1,12 @@
-import board
-import adafruit_dht
-import time
+import Adafruit_DHT as dht
+from time import sleep
 
+DHT = 2
 
-def get_temperature():
-
-    dht = adafruit_dht.DHT22(board.D2)
-
+def read_temperature:
     while True:
-        try:
-            temperature = dht.temperature
-            humidity = dht.humidity
-            # Print what we got to the REPL
-            print("Temp: {:.1f} *C \t Humidity: {}%".format(temperature, humidity))
-        except RuntimeError as e:
-            # Reading doesn't always work! Just print error and we'll try again
-            print("Reading from DHT failure: ", e.args)
-
-        time.sleep(5)
+        #Read Temp and Hum from DHT22
+        h,t = dht.read_retry(dht.DHT22, DHT)
+        #Print Temperature and Humidity on Shell window
+        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t,h))
+        sleep(5) #Wait 5 seconds and read again

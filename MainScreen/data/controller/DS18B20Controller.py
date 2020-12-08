aa4a2ebb2985d9363e570f48a0b29218a31183cd
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-08 10:23:34 +0100

    Minor change

--- MainScreen/data/controller/DS18B20Controller.py
+++ MainScreen/data/controller/DS18B20Controller.py
@@ -20,5 +20,5 @@ def __read_device_temperature(device):
 
 
 def read_temperature():
-    return round((__read_device_temperature(0) * __read_device_temperature(1)) / 2, 1)
+    return round((__read_device_temperature(0) + __read_device_temperature(1)) / 2, 1)
 

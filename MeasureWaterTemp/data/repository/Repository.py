5e774af7e1fe2d0c872440d2336171f2fc18c53e
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-10 20:20:08 +0100

    Minor changes

--- MeasureWaterTemp/data/repository/Repository.py
+++ MeasureWaterTemp/data/repository/Repository.py
@@ -3,6 +3,7 @@ from MeasureWaterTemp.data.datasource import ApiDataSource, LocalDataSource
 
 
 def track_water_temp():
+    print("Reading")
     LocalDataSource.water_temperature = DS18B20Controller.read_temperature()
     try:
         ApiDataSource.send_water_temperature(LocalDataSource.water_temperature)

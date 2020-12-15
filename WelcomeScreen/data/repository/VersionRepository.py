ad5a030b483a81c611e5167b5c70f5913f325fcd
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-15 19:46:36 +0100

    Increasing version code to 1.5

--- WelcomeScreen/data/repository/VersionRepository.py
+++ WelcomeScreen/data/repository/VersionRepository.py
@@ -3,6 +3,6 @@ from WelcomeScreen.domain.exception.NoVersionException import NoVersionException
 
 def get_version_code():
     try:
-        return "1.4"
+        return "1.5"
     except:
         raise NoVersionException

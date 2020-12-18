b1d3ca5eb8dca7462a33a07500163a4e397b8d43
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-19 00:03:40 +0100

    Implementing equal method in Alert

--- HandleAlerts/domain/model/Alert.py
+++ HandleAlerts/domain/model/Alert.py
@@ -5,3 +5,8 @@ class Alert:
     def __init__(self, timestamp, message):
         self.__timestamp = timestamp
         self.__message = message
+
+    def __eq__(self, other):
+        if not isinstance(other, Alert):
+            return False
+        return self.__timestamp == other.__timestamp and self.__message == other.__message

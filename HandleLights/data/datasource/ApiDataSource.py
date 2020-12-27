499ead1eb4bf0e828f05ea63aa452e976d65f26d
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-27 17:36:47 +0100

    Mino change

--- HandleLights/data/datasource/ApiDataSource.py
+++ HandleLights/data/datasource/ApiDataSource.py
@@ -25,5 +25,5 @@ def get_light_preferences():
         print("No serial exception")
         return LightPreferences("-1", "-1")
 
-    except requests.exceptions.ConnectionError:
+    except:
         raise

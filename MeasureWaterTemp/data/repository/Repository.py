42a16576c3e16a063cbc8d5d4c11431212e4c308
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-28 15:29:43 +0100

    Handling api errores when sending the water temperature measurement

--- MeasureWaterTemp/data/repository/Repository.py
+++ MeasureWaterTemp/data/repository/Repository.py
@@ -1,11 +1,14 @@
+from Core.data.device.NoSerialException import NoSerialException
 from MeasureWaterTemp.data.controller import DS18B20Controller
 from MeasureWaterTemp.data.datasource import ApiDataSource, LocalDataSource
+from MeasureWaterTemp.data.datasource.NoMeasuresApiException import NoMeasuresApiException
 
 
 def track_water_temp():
     LocalDataSource.water_temperature = DS18B20Controller.read_temperature()
     try:
         ApiDataSource.send_water_temperature(LocalDataSource.water_temperature)
-    except Exception:
-        pass
+        return True
+    except (NoMeasuresApiException, NoSerialException):
+        return False
 
--- MeasureWaterTemp/domain/usecase/UseCase.py
+++ MeasureWaterTemp/domain/usecase/UseCase.py
@@ -1,10 +1,27 @@
 from dependency_injector.wiring import Provide, inject
 
+from HandleAlerts.HandleAlertsContainer import HandleAlertsContainer
+from HandleAlerts.data.repository.AlertsRepository import AlertsRepository
 from MeasureWaterTemp.data.repository import Repository
 
 
 class MeasureWaterTempUseCase:
 
-    def track_water_temp(self):
-        Repository.track_water_temp()
+    def __init__(self, alerts_repository: AlertsRepository = Provide[HandleAlertsContainer.alerts_repository]):
+        self.__alerts_repository = alerts_repository
+        self.__api_error_count = 0
 
+    def track_water_temp(self):
+        request_success = Repository.track_water_temp()
+        self.__handle_possible_api_errors(request_success)
+
+    def __handle_possible_api_errors(self, request_success: bool):
+        if not request_success and self.__api_errors_count < 3:
+            self.__api_errors_count += 1
+        else:
+            if self.__api_errors_count > 0:
+                self.__api_errors_count -= 1
+
+        if self.__api_errors_count == 3:
+            self.__alerts_repository.create_local_alert("Measure Error")
+            self.__api_errors_count = 0

46e83e346549b7c6886c40369986a1d63808d50b
pablo <pablo@pablos-MacBook-Pro.local>
2020-12-28 10:11:22 +0100

    Minor change

--- HandleLights/data/datasource/ApiDataSource.py
+++ HandleLights/data/datasource/ApiDataSource.py
@@ -1,27 +1,24 @@
 import requests
 
 from Core.data.device import ReadSerialNumber
+from Core.data.device.NoSerialException import NoSerialException
 from HandleLights.data.datasource.NoApiPreferencesException import NoApiPreferenceException
 from HandleLights.domain.model.LightPreferences import LightPreferences
 
-URI = "http://192.168.0.25:8080/preferences?deviceId=sf-"
-
+API_URI = "http://192.168.0.25:8080/preferences?deviceId=sf-"
 
 def get_light_preferences():
     try:
         serial_number = ReadSerialNumber.get_serial_number()
-        preferences = requests.get(URI + serial_number)
-
-        if preferences.status_code == 200:
-            print("Tenemos preferencias")
+        preferences = requests.get(API_URI + serial_number)
+        if preferences.status_code != 200:
+            raise NoApiPreferenceException
+        else:
             return LightPreferences(preferences.json()['lightsPreferences']['range']['starting'],
                                     preferences.json()['lightsPreferences']['range']['finishing'])
-        else:
-            print("No preferencias")
-            return LightPreferences("-1", "-1")
+
+    except NoSerialException:
+        raise NoSerialException
 
     except requests.exceptions.ConnectionError as e:
         raise NoApiPreferenceException("Oeee")
-
-
-

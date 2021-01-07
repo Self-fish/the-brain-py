f07a38183ff08ee311e8143528fadd10c8a4c12c
pablo <pablo@pablos-MacBook-Pro.local>
2021-01-08 00:00:24 +0100

    Minor change

--- Core/data/device/LCDStatus.py
+++ Core/data/device/LCDStatus.py
@@ -2,11 +2,11 @@ from enum import Enum
 
 
 class LCDStatus(Enum):
-    NONE = 0
+    NONE = 0,
     WELCOME_SCREEN = 1,
     MAIN_SCREEN = 2,
     ALERTS_ADVICE_SCREEN = 3,
-    SPECIFIC_ALERT = 4
+    SPECIFIC_ALERT = 4,
     MENU = 5
 
 

59bfb80f2508d362f298151c6f7d0339abb8292b
pablo <pablo@pablos-MacBook-Pro.local>
2021-01-07 21:27:18 +0100

    Prit value instead of Emum itself

--- HandleMenu/domain/usecase/DisplayMenuUseCase.py
+++ HandleMenu/domain/usecase/DisplayMenuUseCase.py
@@ -16,4 +16,4 @@ class DisplayMenuUseCase:
         if len(self.__alerts_repository.get_alerts()) != 0:
             self.__menu_options.insert(0, MenuOptions.SHOW_ALERTS)
         for option in self.__menu_options:
-            print(option)
+            print(option.value)

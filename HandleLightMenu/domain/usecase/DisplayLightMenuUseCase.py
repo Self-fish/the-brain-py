d3f39a9ed1fa1626696ccf1babb3ff8ea3206b54
pablo <pablo@pablos-MacBook-Pro.local>
2021-01-08 20:00:20 +0100

    Making the LightMenu to extends from MenuUseCase

--- HandleLightMenu/domain/usecase/DisplayLightMenuUseCase.py
+++ HandleLightMenu/domain/usecase/DisplayLightMenuUseCase.py
@@ -1,6 +1,7 @@
 from dependency_injector.wiring import Provide, inject
 
 from Core.data.driver.RelayStatus import RelayStatus
+from CoreMenu.domain.usecase.MenuUseCase import MenuUseCase
 from HandleLightMenu.domain.model.LightMenuOptions import LightMenuOptions
 from HandleLights.HandleLightsContainer import HandleLightsContainer
 from HandleLights.data.repository.LightStatus import LightStatusRepository
@@ -8,26 +9,22 @@ from MainScreen.MainScreenContainer import MainScreenContainer
 from MainScreen.data.controller.LCDController import MainScreenController
 
 
-class DisplayLightMenuUseCase:
+@MenuUseCase.register
+class DisplayLightMenuUseCase(MenuUseCase):
 
     @inject
     def __init__(self, screen_controller: MainScreenController = Provide[MainScreenContainer.main_screen_controller],
                  light_repository: LightStatusRepository = Provide[HandleLightsContainer.light_status_repository]):
+        super().__init__(screen_controller)
         self.__screen_repository = screen_controller
-        self.__menu_options = [LightMenuOptions.AUTOMATIC, LightMenuOptions.MANUAL_OFF]
         self.__light_repository = light_repository
-        self.__selected_option = 0
 
-    def display_light_menu(self):
-        self.__build_menu()
-        self.__screen_repository.print_menu(self.__menu_options, self.__menu_options[self.__selected_option])
+    def build_menu_options(self):
+        self.menu_options = [LightMenuOptions.AUTOMATIC]
+        self.menu_options.append(self.__light_status_to_display())
 
-    def __build_menu(self):
-        if self.__menu_options.__contains__(LightMenuOptions.MANUAL_OFF):
-            self.__menu_options.remove(LightMenuOptions.MANUAL_OFF)
-        if self.__menu_options.__contains__(LightMenuOptions.MANUAL_ON):
-            self.__menu_options.remove(LightMenuOptions.MANUAL_ON)
-        self.__menu_options.append(self.__light_status_to_display())
+    def select_option(self):
+        pass
 
     def __light_status_to_display(self):
         if self.__light_repository.current_light_status == RelayStatus.ON:

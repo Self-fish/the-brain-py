2160ca0775ec2aaae5765eb71a741fb7be30012d
pablo <pablo@pablos-MacBook-Pro.local>
2021-01-08 19:55:11 +0100

    Minor changes

--- CoreMenu/domain/usecase/MenuUseCase.py
+++ CoreMenu/domain/usecase/MenuUseCase.py
@@ -10,8 +10,8 @@ class MenuUseCase(abc.ABC):
 
     def __init__(self, screen_controller: MainScreenController):
         self.__screen_controller = screen_controller
-        self.__menu_options = None
-        self.__selected_option = 0
+        self.menu_options = None
+        self.selected_option = 0
 
     @abc.abstractmethod
     def build_menu_options(self):
@@ -27,20 +27,20 @@ class MenuUseCase(abc.ABC):
         self.__print_menu()
         time.sleep(1)
         self.__wait_joystick_interaction()
-        self.__menu_options = None
-        self.__selected_option = 0
+        self.menu_options = None
+        self.selected_option = 0
 
     def __wait_joystick_interaction(self):
         should_wait = True
         while should_wait:
             if JoystickController.is_joystick_down():
-                if self.__selected_option != len(self.__menu_options) - 1:
-                    self.__selected_option += 1
+                if self.selected_option != len(self.menu_options) - 1:
+                    self.selected_option += 1
                     self.__print_menu()
                     time.sleep(1)
             elif JoystickController.is_joystick_up():
-                if self.__selected_option != 0:
-                    self.__selected_option -= 1
+                if self.selected_option != 0:
+                    self.selected_option -= 1
                     self.__print_menu()
                     time.sleep(1)
             elif JoystickController.is_switch_pressed():
@@ -48,5 +48,5 @@ class MenuUseCase(abc.ABC):
                 self.select_option()
 
     def __print_menu(self):
-        self.__screen_controller.print_menu(self.__menu_options, self.__menu_options[self.__selected_option])
+        self.__screen_controller.print_menu(self.menu_options, self.menu_options[self.selected_option])
 

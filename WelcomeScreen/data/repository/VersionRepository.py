4dc38d44053baa47de98dbb43a35aedabde70336
pablo <pablo@pablos-MacBook-Pro.local>
2021-01-09 17:24:39 +0100

    Increasing Version Code

--- WelcomeScreen/data/repository/VersionRepository.py
+++ WelcomeScreen/data/repository/VersionRepository.py
@@ -3,6 +3,6 @@ from WelcomeScreen.domain.exception.NoVersionException import NoVersionException
 
 def get_version_code():
     try:
-        return "1.9"
+        return "1.10"
     except:
         raise NoVersionException

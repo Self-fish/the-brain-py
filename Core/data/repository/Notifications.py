ff8a68833099f4e749b22e0cbeebe4da062243d9
pablo <pablo@pablos-MacBook-Pro.local>
2021-02-28 19:22:13 +0100

    Testing notifications api

--- Core/data/repository/Notifications.py
+++ Core/data/repository/Notifications.py
@@ -6,7 +6,8 @@ API_URI = "http://192.168.0.15:8085/notifications/"
 def create_notification(message):
     try:
         body = message
-        requests.post(API_URI, json=body)
+        response = requests.post(API_URI, json=body)
+        print(response.status_code)
     except Exception:
         print("Oh nooo!")
         pass

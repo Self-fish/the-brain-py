027d7bb5917fb2ac8d62016fafda51fa3ac01cf4
pablo <pablo@pablos-MacBook-Pro.local>
2021-02-28 10:57:17 +0100

    Increasing version to 1.12

--- WelcomeScreen/data/repository/VersionRepository.py
+++ WelcomeScreen/data/repository/VersionRepository.py
@@ -3,6 +3,6 @@ from WelcomeScreen.domain.exception.NoVersionException import NoVersionException
 
 def get_version_code():
     try:
-        return "1.11"
+        return "1.12"
     except:
         raise NoVersionException

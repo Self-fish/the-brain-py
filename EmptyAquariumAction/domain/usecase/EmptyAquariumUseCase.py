a1c0796460b4861640f8d93cc8347fd573c265c1
pablo <pablo@pablos-MacBook-Pro.local>
2021-03-10 21:01:07 +0100

    MInor chanages

--- EmptyAquariumAction/domain/usecase/EmptyAquariumUseCase.py
+++ EmptyAquariumAction/domain/usecase/EmptyAquariumUseCase.py
@@ -5,6 +5,9 @@ from EmptyAquariumAction.data.controller import MCP3008Controller
 class EmptyAquariumUseCase(CoreActionUseCase):
 
     def execute_action(self):
-        while True:
+        i = 0
+        while i < 10:
             distance = MCP3008Controller.calculate_distance()
             print(distance)
+            i = i + 1
+

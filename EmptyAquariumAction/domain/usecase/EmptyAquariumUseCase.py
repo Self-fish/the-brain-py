78f6dae4d4262ca2948bf0b1eca9fc004d8a9cbf
pablo <pablo@pablos-MacBook-Pro.local>
2021-03-14 18:10:32 +0100

    Testing distance sensor

--- EmptyAquariumAction/domain/usecase/EmptyAquariumUseCase.py
+++ EmptyAquariumAction/domain/usecase/EmptyAquariumUseCase.py
@@ -5,9 +5,5 @@ from EmptyAquariumAction.data.controller import MCP3008Controller
 class EmptyAquariumUseCase(CoreActionUseCase):
 
     def execute_action(self):
-        i = 0
-        while i < 10:
-            distance = MCP3008Controller.calculate_distance()
-            print(distance)
-            i = i + 1
-
+        distance = MCP3008Controller.calculate_distance()
+        print(distance)

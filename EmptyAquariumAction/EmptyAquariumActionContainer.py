5cc8e839080a9328903a150e2510433a5c772b9e
pablo <pablo@pablos-MacBook-Pro.local>
2021-03-15 20:44:43 +0100

    Creating the repository that switch on or off the empty pump and adding he logic in the use case

--- EmptyAquariumAction/EmptyAquariumActionContainer.py
+++ EmptyAquariumAction/EmptyAquariumActionContainer.py
@@ -1,10 +1,13 @@
 from dependency_injector import containers, providers
 
 from Core.data.driver.RelayController import RelayController
+from EmptyAquariumAction.data.repository.EmptyPumpRepository import EmptyPumpRepository
 from EmptyAquariumAction.data.repository.FilterRepository import FilterRepository
 
 
 class EmptyAquariumActionContainer(containers.DeclarativeContainer):
     config = providers.Configuration()
     filter_controller = providers.Factory(RelayController, 1)
+    empty_pump_controller = providers.Factory(RelayController, 5)
     filter_repository = providers.Singleton(FilterRepository, filter_controller)
+    empty_pump_repository = providers.Singleton(EmptyPumpRepository, empty_pump_controller)
--- EmptyAquariumAction/domain/usecase/EmptyAquariumUseCase.py
+++ EmptyAquariumAction/domain/usecase/EmptyAquariumUseCase.py
@@ -4,14 +4,18 @@ from Core.data.driver.RelayStatus import RelayStatus
 from Core.domain.usecases.CoreActionUseCase import CoreActionUseCase
 from EmptyAquariumAction.EmptyAquariumActionContainer import EmptyAquariumActionContainer
 from EmptyAquariumAction.data.controller import MCP3008Controller
+from EmptyAquariumAction.data.repository.EmptyPumpRepository import EmptyPumpRepository
 from EmptyAquariumAction.data.repository.FilterRepository import FilterRepository
 
 
 class EmptyAquariumUseCase(CoreActionUseCase):
 
     @inject
-    def __init__(self, filter_repository: FilterRepository = Provide[EmptyAquariumActionContainer.filter_repository]):
+    def __init__(self, filter_repository: FilterRepository = Provide[EmptyAquariumActionContainer.filter_repository],
+                 empty_pump_repository: EmptyPumpRepository =
+                 Provide[EmptyAquariumActionContainer.empty_pump_repository]):
         self.__filter_repository = filter_repository
+        self.__empty_pump_repository = empty_pump_repository
 
     def execute_action(self):
         # 1. Encender el calendator del cubo de llenado
@@ -20,9 +24,9 @@ class EmptyAquariumUseCase(CoreActionUseCase):
         # 4. Parar el calentador del cubo de llenado
 
         self.__filter_repository.switch_filter_off()
-
-
-
-        while True:
+        self.__empty_pump_repository.switch_pump_on()
+        distance = 0
+        while distance < 11:
             distance = MCP3008Controller.calculate_distance()
             print(distance)
+        self.__empty_pump_repository.switch_pump_off()

7a8d3074e11b0c273e32a967ff2bc5bfb907a950
pablo <pablo@pablos-MacBook-Pro.local>
2021-03-19 13:29:20 +0100

    Moving the Water temperature controller to the core module

--- MeasureWaterTemp/data/repository/MeasureWaterRepository.py
+++ MeasureWaterTemp/data/repository/MeasureWaterRepository.py
@@ -1,7 +1,7 @@
 import time
 
 from Core.data.device.NoSerialException import NoSerialException
-from MeasureWaterTemp.data.controller import DS18B20Controller
+from Core.data.driver.DS18B20Controller import DS18B20Controller
 from MeasureWaterTemp.data.datasource import ApiDataSource, LocalDataSource
 from MeasureWaterTemp.data.datasource.NoMeasuresApiException import NoMeasuresApiException
 
@@ -10,12 +10,13 @@ class MeasureWaterRepository:
 
     ONE_MINUTE = 60000
 
-    def __init__(self):
+    def __init__(self, water_temperature_controller):
         self.__last_measure_sent = 0
+        self.__water_temperature_controller: DS18B20Controller = water_temperature_controller
 
     def track_water_temp(self):
         current_time = time.time() * 1000
-        LocalDataSource.water_temperature = DS18B20Controller.read_temperature()
+        LocalDataSource.water_temperature = self.__water_temperature_controller.read_device_temperature()
         if current_time > self.__last_measure_sent + self.ONE_MINUTE:
             try:
                 ApiDataSource.send_water_temperature(LocalDataSource.water_temperature)

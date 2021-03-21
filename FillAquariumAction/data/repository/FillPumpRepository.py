32be6f8a3b69cfd79160ee70518e8694170ffb53
pablo <pablo@pablos-MacBook-Pro.local>
2021-03-21 23:26:24 +0100

    Minor changes

--- FillAquariumAction/data/repository/FillPumpRepository.py
+++ FillAquariumAction/data/repository/FillPumpRepository.py
@@ -6,14 +6,17 @@ class FillPumpRepository:
 
     def __init__(self, relay_controller: RelayController):
         self.__relay_controller = relay_controller
+        self.__relay_controller.update_relay_status(RelayStatus.OFF)
         self.__pump_status = RelayStatus.OFF
 
     def switch_pump_off(self):
+        print("Apagamos la bomba")
         if self.__pump_status != RelayStatus.OFF:
             self.__relay_controller.update_relay_status(RelayStatus.OFF)
             self.__pump_status = RelayStatus.OFF
 
     def switch_pump_on(self):
+        print("Encendemos la bomba")
         if self.__pump_status != RelayStatus.ON:
             self.__relay_controller.update_relay_status(RelayStatus.ON)
             self.__pump_status = RelayStatus.ON

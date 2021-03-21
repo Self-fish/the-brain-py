4375d6b60f202d8d384e815ba54a792a987f5743
pablo <pablo@pablos-MacBook-Pro.local>
2021-03-21 21:20:41 +0100

    Minor changes

--- EmptyAquariumAction/domain/usecase/EmptyAquariumUseCase.py
+++ EmptyAquariumAction/domain/usecase/EmptyAquariumUseCase.py
@@ -31,7 +31,8 @@ class EmptyAquariumUseCase(CoreActionUseCase):
         self.__filter_repository.switch_filter_off()
         original_distance = MCP3008Controller.calculate_distance()
         print("Original distance: " + str(original_distance))
-        distance = 99
+        print(original_distance + 4.5)
+        distance = 0
         self.__empty_pump_repository.switch_pump_on()
 
         while distance > original_distance + 4.5:
